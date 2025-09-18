74a33aefae1f9710af44ff0a0ed6171f9b5e245c
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-09-18 07:19:22 +0600

    Add

---
+++ Add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>  
+using namespace std; 
+
+int main() {
+    int x, y;        
+    cin >> x >> y;   
+
+    int total = x + y; 
+    cout << total << endl;
+
+    return 0; 
+}
