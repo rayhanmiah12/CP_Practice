76e68e76d1fa9588bf148ea4674ab743270a970c
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-09-29 19:50:04 +0600

    Donation_Rewards

---
+++ Donation_Rewards.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        if(x<=3)
+        {
+            cout << "BRONZE" << endl;
+        }
+        else if(x<=6)
+        {
+            cout << "SILVER" << endl;
+        }
+        else{
+            cout << "GOLD" << endl;
+        }
+    }
+}
