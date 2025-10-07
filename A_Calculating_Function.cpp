ca9f545fb2998046c7e240bbad8cbb22d48eccb4
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-10-07 22:38:05 +0600

    A_Calculating_Function

---
+++ A_Calculating_Function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a;
+    cin >> a;
+    
+    if (a % 2 == 0) 
+    {
+        cout << a/2 << endl;
+    } else {
+        cout << -(a + 1) / 2 << endl;
+    }
+    return 0;
+}
