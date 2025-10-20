f50474743c8783e957d95cc2593fe636a81d294b
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-10-21 03:08:39 +0600

    A_Petya_and_Strings

---
+++ A_Beautiful_Year.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int y;
+    cin>>y;
+    if(y<=1987)
+    {
+        cout << "2013" << endl;
+    }
+    else
+    {
+        cout << "2014" << endl;
+    }
+}
