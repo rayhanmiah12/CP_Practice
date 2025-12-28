cea5b5b69a87040c0c3f140256998d301bf85844
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-12-28 21:09:18 +0600

    A_Takahashi

---
+++ A_Takahashi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (c >= a && d < b) 
+    {
+        cout << "Yes" << endl;  
+    } 
+    else
+    {
+        cout << "No" << endl;   
+    }
+}
