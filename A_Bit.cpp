38ca05446ca7033b9cb7222c6dc4e0b71a4681ae
rayhanmiah12 <rayhanmiah5562@gmail.com>
2026-05-08 23:10:20 +0600

    A_Bit.cpp

---
+++ A_Bit.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,x=0;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        if(s[1] == '+')
+            x++;
+        else
+            x--;
+    }
+    cout << x;
+    return 0;
+}
